0a973c15ebd1377daca6241b2c9689eca71a65e8
Santiago Romero <sromero@Feather.local>
2016-02-17 12:18:46 -0300

    Heroku config

--- mozio_test/settings.py
+++ mozio_test/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
